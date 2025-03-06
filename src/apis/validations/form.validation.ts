06f3b6504cab1bbf484c686f06cba4bac6d88c82
Muhammad Talha <78800045+mtalha6717@users.noreply.github.com>
2025-03-06 14:57:20 +0500

    backend validations

--- src/apis/validations/form.validation.ts
+++ src/apis/validations/form.validation.ts
@@ -3,16 +3,12 @@ import * as Joi from 'joi'
 
 const addFormSchema = {
   body: Joi.object().keys({
-    name: Joi.string().required(),
     email: Joi.string().email().required(),
     phone: Joi.string().required(),
-    college:
-      Joi.string()
-      .required(),
+    college: Joi.string().required(),
     type: Joi.string()
       .valid(...Object.values(FormType))
       .default('University'),
-    description: Joi.string().required(),
     userLatitude: Joi.number().optional(),
     userLongitude: Joi.number().optional(),
     collegeLatitude: Joi.number().optional(),
